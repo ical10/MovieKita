da3a91f93812aeaa1a16c7836dd3ff2fbc95a320
ical10 <mochhusnir@gmail.com>
2021-03-18 13:45:53 +0700

    Initial Commit

---
+++ MovieKitaTests/MovieKitaTests.swift
@@ -0,0 +1,34 @@
+//
+//  MovieKitaTests.swift
+//  MovieKitaTests
+//
+//  Created by Rizal on 18/03/2021.
+//  Copyright © 2021 Rizal. All rights reserved.
+//
+
+import XCTest
+@testable import MovieKita
+
+class MovieKitaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
